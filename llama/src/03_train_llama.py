e5aee5b0123d958fbe2021e8a7f41bce50b4c30c
Paul Keller <contact.keller.paul@gmail.com>
2023-07-19 10:15:19 +0200

    fixed requirements and jobs

--- llama/src/03_train_llama.py
+++ llama/src/03_train_llama.py
@@ -147,7 +147,7 @@ def main():
     # Log on each process the small summary:
     logger.warning(
         f"Process rank: {training_args.local_rank}, device: {training_args.device}, n_gpu: {training_args.n_gpu}"
-        + f"distributed training: {bool(training_args.local_rank != -1)}, 16-bits training: {training_args.fp16}"
+        + f" distributed training: {bool(training_args.local_rank != -1)}, 16-bits training: {training_args.fp16}"
     )
     logger.info(f"Training/evaluation parameters {training_args}")
     
@@ -178,7 +178,7 @@ def main():
     dataset_args["keep_linebreaks"] = data_args.keep_linebreaks
     raw_datasets = load_dataset(
         extension,
-        datafiles=data_files,
+        data_files=data_files,
         **dataset_args,
     )
     raw_datasets["validation"] = load_dataset(
