f21edf8c625fdedf7316dbd5106e5610d9cfe112
Paul Keller <contact.keller.paul@gmail.com>
2023-08-10 16:12:21 +0200

    added comments to prediction script

--- llama/src/03_train_lora.py
+++ llama/src/03_train_lora.py
@@ -17,6 +17,7 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 model_name = "meta-llama/Llama-2-7b-hf"
 token = "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"
 
+# initialize bitsanbytes configuration
 bnb_config = BitsAndBytesConfig(
     load_in_4bit=True,
     bnb_4bit_use_double_quant=True,
@@ -24,8 +25,10 @@ bnb_config = BitsAndBytesConfig(
     bnb_4bit_compute_dtype=torch.float16
 )
 
+# log into huggingface hub
 huggingface_hub.login(token=token) 
 
+# load the llama2 model with 8bit
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
     load_in_8bit=True,
@@ -33,7 +36,7 @@ model = AutoModelForCausalLM.from_pretrained(
 )
 
 print(f"Setting special tokens for LLaMA model {model_name}...")
-
+# set special tokens for padding, begin of sequence and end of sequence
 model.config.pad_token_id=0
 model.config.bos_token_id=1
 model.config.eos_token_id=2
@@ -41,6 +44,7 @@ model.config.eos_token_id=2
 print(f"Loaded model {model_name}")
 tokenizer = LlamaTokenizer.from_pretrained(model_name)
 print(f"Setting special tokens for LLaMA tokenizer {model_name}...")
+# and apply same tokens to tokenizer
 tokenizer.pad_token_id=0
 tokenizer.bos_token_id=1
 tokenizer.eos_token_id=2
@@ -48,6 +52,7 @@ tokenizer.padding_side = "left"
 
 print(f"Loaded tokenizer {model_name}")
 
+# prints number of parameters that will be trained
 def print_trainable_parameters(model):
     trainable_params = 0
     all_param = 0
@@ -59,7 +64,7 @@ def print_trainable_parameters(model):
         f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
     )
     
-
+# define lora configuration
 config = LoraConfig(
     r=16,
     lora_alpha=32,
@@ -69,10 +74,13 @@ config = LoraConfig(
     task_type="CAUSAL_LM"
 )
 
+# prepare the model for kbit training
 model = prepare_model_for_kbit_training(model)
+# transform it to a peft model
 model = get_peft_model(model, config)
 print_trainable_parameters(model)
 
+# and load in dataset (no evaluation done here)
 data_files = {}
 dataset_args = {}
 data_files["train"] =  "./input/health_information_systems_epub.md"
@@ -85,9 +93,11 @@ raw_dataset = load_dataset(
     **dataset_args
 )
 
+# extract features from dataset
 column_names = list(raw_dataset["train"].features)
 text_column_name = "text" if "text" in column_names else column_names[0]
 
+# tokenize the dataset
 def tokenizer_function(examples):
     return tokenizer(examples[text_column_name])
 tokenized_datasets = raw_dataset.map(
@@ -98,8 +108,10 @@ tokenized_datasets = raw_dataset.map(
     load_from_cache_file=True,
     desc="Running tokenizer on dataset"
 )
+# limit blocksize to 1024 tokens
 block_size = min(1024, tokenizer.model_max_length)
 
+# group the text into block size chunks
 def group_texts(examples):
     concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
     total_length = len(concatenated_examples[list(examples.keys())[0]])
@@ -117,8 +129,10 @@ lm_datasets = tokenized_datasets.map(
     load_from_cache_file=True,
     desc=f"Grouping texts in chunks of {block_size} tokens"
 )
+
 train_dataset = lm_datasets["train"]
 
+# initialize trainer
 trainer = transformers.Trainer(
     model=model,
     train_dataset=train_dataset,
@@ -135,10 +149,7 @@ trainer = transformers.Trainer(
     ),
     data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False)
 )
-
-
-
-
+# adjust model special tokens
 print(f"Setting special tokens for LLaMA model {model_name}...")
 model.config.pad_token_id = 0
 model.config.bos_token_id = 1
@@ -150,13 +161,19 @@ tokenizer.bos_token_id = 1
 tokenizer.eos_token_id = 2
 
 model.config.use_cache = False
+# casts the model to the right device
 with torch.autocast("cuda"):
+    # and train it
     train_result = trainer.train()
+# save the model to the specified output directory
 trainer.save_model()
 
+# calculate metrics
 metrics = train_result.metrics
 metrics["train_samples"] = len(train_dataset)
-        
+
+# log and save the metrics
 trainer.log_metrics("train", metrics)
 trainer.save_metrics("train", metrics)
+# save the state of the trainer for future resume
 trainer.save_state()
--- llama/src/04_predict_llama2_base.py
+++ llama/src/04_predict_llama2_base.py
@@ -9,14 +9,20 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 import huggingface_hub
 
+# generate on CUDA / GPU if available
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+# setup logging
 logger = logging.getLogger(__name__)
 
 def main():
+    # parse arguments
     parser = argparse.ArgumentParser()
+    # used for downloading llama2 model from huggingface hub
     parser.add_argument("-t", "--token", type=str, default="", help="Huggingface token")
+    # path to the model directory / model name
     parser.add_argument("-m", "--model-dir", type=str, default="./trained/7B", help="Path to the model directory")
+    # path to the output file
     parser.add_argument("-o", "--output-path", type=str, default="./output/generated.csv", help="Path to the output file")
     
     args = parser.parse_args()
@@ -27,7 +33,7 @@ def main():
         datefmt="%Y/%m/%d %H:%M:%S",
         handlers=[logging.StreamHandler(sys.stdout)],
     )
-    
+    # set logging verbosity to info
     transformers.utils.logging.set_verbosity_info()
     
     log_level = logging.INFO
@@ -36,14 +42,12 @@ def main():
     transformers.utils.logging.enable_default_handler()
     transformers.utils.logging.enable_explicit_format()
 
-    #single_question_dataset = pd.read_csv("data/single_questions.csv")
-    #multi_question_dataset = pd.read_csv("data/multi_questions.csv")
-    #transfer_question_dataset = pd.read_csv("data/transfer_questions.csv")
-    
+    # read in questions from json files
     single_question_dataset = read_json("data/single_questions.json")
     multi_question_dataset = read_json("data/multi_questions.json")
     transfer_question_dataset = read_json("data/transfer_questions.json")
     
+    # prepare dataframe for output
     dataframe = pd.DataFrame(columns=["question", "transformed", "generated", "true_answer", "num_answers", "type", "source", "context", "true_input"])
     
     model_dir = args.model_dir
@@ -52,51 +56,64 @@ def main():
     print("Loading Model from", model_dir)
     print("Saving output to", output_file)
     
+    # load in model and port to device
     model = AutoModelForCausalLM.from_pretrained(model_dir, use_auth_token=True)
     model = model.to(device)
     tokenizer = AutoTokenizer.from_pretrained(model_dir, use_auth_token=True)
     
+    # introduce special tokens
     tokenizer.pad_token_id = 0
     tokenizer.bos_token_id = 1
     tokenizer.eos_token_id = 2
     
+    # and generate answers to each question file
     generate_for_single_csv(tokenizer, model, single_question_dataset, "single", dataframe)
     generate_for_single_csv(tokenizer, model, multi_question_dataset, "multi", dataframe)
     generate_for_single_csv(tokenizer, model, transfer_question_dataset, "transfer", dataframe)
     
+    # save the results
     dataframe.to_csv(output_file, index=False)
 
 def read_json(file):
+    # read in json
     with open(file, "r", encoding="utf-8") as f:
         data = json.load(f)
+    # and dump into pandas dataframe
     df = pd.DataFrame(columns=["question", "transformed", "true_answer", "num_answers", "source", "context"])
     for item in data:
         df.loc[len(df)] = [item["question"], item["transformed"], item["true_answer"], item["num_answers"], item["source"], item["context"]]
     return df
 
 def generate_for_single_csv(tokenizer: transformers.PreTrainedTokenizer | transformers.PreTrainedTokenizerFast, model, csv_df: pd.DataFrame, csv_type: str, output_df: pd.DataFrame):
+    # for each row in the dataframe of questions
     for index, row in csv_df.iterrows():
+        # print progress
         print(f"{index}/{len(csv_df)} Questions")
         print(f"Generation {index}/{len(csv_df)}")
+        # extract question data
         question = row["question"]
         transformed_question = row["transformed"]
         true_answer = row["true_answer"]
         num_answers = row["num_answers"]
         source = row["source"]
         context = row["context"]
-        
+        # generate prompt depending if context is available or not
         if context != "":
             prompt = f"Instruction: You are given a question and a context. Answer the question to your best knowledge.\nQuestion: {transformed_question}\nContext: {context}\nAnswer: "
         else:
             prompt = f"Instruction: You are given a question. Answer the question to your best knowledge.\nQuestion: {transformed_question}\nAnswer: "
         
         print(prompt)
+        # tokenize prompt and port to device
         inputs = tokenizer(prompt, return_tensors="pt")
         inputs = inputs.to(device)
+        # generate answers (temperature is doing nothing here?)
         output = model.generate(inputs.input_ids, temperature=0.9, max_new_tokens=512)
+        # decode the output
         generated = tokenizer.batch_decode(output, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
         generated = generated[len(prompt):]
         print(f"Generated: {generated}")
+        # and save the output
         output_df.loc[len(output_df)] = [question, transformed_question, generated, true_answer, num_answers, csv_type, source, context, prompt]
 
 if __name__ == "__main__":
