a664d9a76516c7ffd2314db928e643ed759ad2ee
Paul Keller <contact.keller.paul@gmail.com>
2023-08-25 14:49:30 +0200

    HEAT MAPS

--- llama/src/06_evaluation_correctness.py
+++ llama/src/06_evaluation_correctness.py
@@ -1,3 +1,4 @@
+import json
 import os
 import json_fix # this import is needed for def __json__(self) although not used
 import argparse
@@ -8,7 +9,8 @@ from question import Question, QuestionSource, QuestionType
 
 from pandas import DataFrame
 import pandas as pd
-from model_helper import get_model_key, get_model_name
+import seaborn
+from model_helper import get_model_key, get_model_name, ModelHeatMap
 
 
 
@@ -20,15 +22,16 @@ def main():
                         default="evaluation/input")
     # path to output directory
     parser.add_argument("-o", "--output", type=str, help="Path to the output directory", default="evaluation/criterias/correctness")
-    parser.add_argument("-s", "--skip", type=bool, help="Skip recalculation of statistics", default=False)
+    parser.add_argument("-s", "--skip", action="store_true", help="Skip recalculation of statistics")
     args = parser.parse_args()
     
     if not args.skip:
-        df = calculate_correctness(args)
+        df, heatmap_data = calculate_correctness(args)
     else:
         df = pd.read_csv(args.output + "/evaluation.csv")
+        heatmap_data = ModelHeatMap.read_json(args.output + "/heatmap.json")
     
-
+    
     # get model names for plots
     model_names = df["Model"].tolist()
     
@@ -37,6 +40,8 @@ def main():
     num_wrong = np.array(df["Num_Wrong"].tolist())
     num_unanswered = np.array(df["Num_Unanswered"].tolist())
     show_answer_bars(num_correct, num_wrong, num_unanswered, model_names, "Richtig, Falsch und Unbeantwortete Fragen", args.output+"/answers_total.png")
+    for model in model_names:
+        show_heatmap(heatmap_data.get_heatmap(model, "makrof1"), f"Makro F1 ({model})", args.output+f"/makrof1_{model}_heat.png")
     
     for type in QuestionType:
         num_correct = np.array(df[f"Num_Correct_{type}"].tolist())
@@ -67,6 +72,22 @@ def calculate_correctness(args):
             unsorted_models[name] = Question.read_json(os.path.join(args.data, file))
     model_names = sorted(unsorted_models.keys(), key=get_model_key)
     models: list[list[Question]] = [unsorted_models[name] for name in model_names]
+    
+    heatmap_data: ModelHeatMap = ModelHeatMap()
+    for model in model_names:
+        for type in QuestionType:
+            for source in QuestionSource:
+                heatmap_data.set_value(model, type, source, "correct", 0)
+                heatmap_data.set_value(model, type, source, "wrong", 0)
+                heatmap_data.set_value(model, type, source, "unanswered", 0)
+                heatmap_data.set_value(model, type, source, "makrof1", 0)
+                heatmap_data.set_value(model, type, source, "f1", [])
+        # for source in QuestionSource:
+        #     heatmap_data.set_value(model, "total", source, "correct", 0)
+        #     heatmap_data.set_value(model, "total", source, "wrong", 0)
+        #     heatmap_data.set_value(model, "total", source, "unanswered", 0)
+        #     heatmap_data.set_value(model, "total", source, "makrof1", 0)
+        #     heatmap_data.set_value(model, "total", source, "f1", [])
     
     # initialize pandas dataframe
     dataframe_columns = ["Model", "Num_Correct", "Num_Wrong", "Num_Unanswered", "Num_Questions", "MacroF1"]
@@ -137,20 +158,28 @@ def calculate_correctness(args):
             f1_scores.append(f1)
             type_f1_scores[QuestionType(question.type)].append(f1) # type: ignore
             source_f1_scores[QuestionSource(question.source)].append(f1) # type: ignore
+            heatmap_data.append_value(name, question.type, question.source, "f1", f1)
+            # heatmap_data.append_value(name, "total", question.source, "f1", f1)
             # increase counters depending on question type
             # and if question was answered correctly, wrong or not at all
             if(question.answered == 0):
                 num_unanswered += 1
                 type_num_unanswered[QuestionType(question.type)] += 1 # type: ignore
                 source_num_unanswered[QuestionSource(question.source)] += 1 # type: ignore
+                heatmap_data.update_value(name, question.type, question.source, "unanswered", 1)
+                # heatmap_data.update_value(name, "total", question.source, "unanswered", 1)
             elif(question.answered == 1):
                 num_wrong += 1
                 type_num_wrong[QuestionType(question.type)] += 1 # type: ignore
                 source_num_wrong[QuestionSource(question.source)] += 1 # type: ignore
+                heatmap_data.update_value(name, question.type, question.source, "wrong", 1)
+                # heatmap_data.update_value(name, "total", question.source, "wrong", 1)
             elif(question.answered == 2):
                 num_correct += 1
                 type_num_correct[QuestionType(question.type)] += 1 # type: ignore
                 source_num_correct[QuestionSource(question.source)] += 1 # type: ignore
+                heatmap_data.update_value(name, question.type, question.source, "correct", 1)
+                # heatmap_data.update_value(name, "total", question.source, "correct", 1)
         
         # calcualte macro f1 scores
         macro_f1 = sum(f1_scores) / len(f1_scores)
@@ -158,8 +187,16 @@ def calculate_correctness(args):
         source_macro_f1: dict[QuestionSource, float] = {}
         for type in QuestionType:
             type_macro_f1[type] = sum(type_f1_scores[type]) / len(type_f1_scores[type])
+            for source in QuestionSource:
+                type_f1: list[int] = heatmap_data.get_value(name, type, source, "f1") # type: ignore
+                type_makro_f1 = sum(type_f1) / len(type_f1)
+                heatmap_data.set_value(name, type, source, "makrof1", type_makro_f1)
         for source in QuestionSource:
             source_macro_f1[source] = sum(source_f1_scores[source]) / len(source_f1_scores[source])
+            # total_f1: list[int] = heatmap_data.get_value(name, "total", source, "f1") # type: ignore
+            # total_makro_f1 = sum(total_f1) / len(total_f1)
+            # heatmap_data.set_value(name, "total", source, "makrof1", total_makro_f1)
+
 
         # and the total amount of questions per type
         questions = num_correct + num_wrong + num_unanswered
@@ -169,6 +206,7 @@ def calculate_correctness(args):
             type_questions[type] = type_num_correct[type] + type_num_wrong[type] + type_num_unanswered[type]
         for source in QuestionSource:
             source_questions[source] = source_num_correct[source] + source_num_wrong[source] + source_num_unanswered[source]
+            
         # add to dataframe
         final_results = [name, num_correct, num_wrong, num_unanswered, questions, macro_f1]
         final_results.extend([type_num_correct[type] for type in QuestionType])
@@ -185,7 +223,22 @@ def calculate_correctness(args):
     
     # save data to csv
     df.to_csv(args.output + "/evaluation.csv", index=False)
-    return df
+    heatmap_data.save_json(args.output + "/heatmap.json")
+    return df, heatmap_data
+
+def show_heatmap(heatmap_data: tuple[list[str], list[str], list[list[float]]], title: str, file_name: str):
+    y_labels = heatmap_data[0]
+    x_labels = heatmap_data[1]
+    values = np.array(heatmap_data[2])
+    fig = plt.figure(figsize=(8,6))
+    axis = fig.subplots()
+    
+    seaborn.heatmap(values, xticklabels=x_labels, yticklabels=y_labels, ax=axis, vmin=1, vmax=0)
+    axis.set_ylabel("Makro F1")
+    axis.set_xlabel("Modelle")
+    axis.set_title(title, pad=15)
+    fig.tight_layout()
+    fig.savefig(file_name)
 
 def show_answer_bars(correct, wrong, unanswered, names, title, file_name):
     # create figure of 20, 10 size
--- llama/src/model_helper.py
+++ llama/src/model_helper.py
@@ -1,3 +1,7 @@
+from typing import Any
+import json_fix
+import json
+
 def get_model_key(name: str) -> int:
     if name == "gpt4":
             return 0
@@ -23,3 +27,104 @@ def get_model_name(file_name: str) -> str:
             return name_split[0] + '_' + name_split[1] + '_' + name_split[2]
         else:
             return name_split[0] + '_' + name_split[1]
+        
+class ModelHeatMap:   
+    def __init__(self, models: dict[str, "TypeHeatMap"] | None = None) -> None:
+        if models is None:
+            self.models: dict[str, TypeHeatMap] = {}
+        else:
+            self.models = models
+    def get_value(self, model_name: str, type_name: str, source_name: str, value_name: str):
+        if model_name not in self.models:
+            return None
+        if type_name not in self.models[model_name].types:
+            return None
+        if source_name not in self.models[model_name].types[type_name].sources:
+            return None
+        if value_name not in self.models[model_name].types[type_name].sources[source_name]:
+            return None
+        return self.models[model_name].types[type_name].sources[source_name][value_name]
+    def set_value(self, model_name: str, type_name: str, source_name: str, value_name: str, value: Any):
+        if model_name not in self.models:
+            self.models[model_name] = TypeHeatMap()
+        if type_name not in self.models[model_name].types:
+            self.models[model_name].types[type_name] = SourceHeatMap({})
+        if source_name not in self.models[model_name].types[type_name].sources:
+            self.models[model_name].types[type_name].sources[source_name] = {}
+        self.models[model_name].types[type_name].sources[source_name][value_name] = value
+    def update_value(self, model_name: str, type_name: str, source_name: str, value_name: str, value: int):
+        if model_name not in self.models:
+            self.models[model_name] = TypeHeatMap()
+        if type_name not in self.models[model_name].types:
+            self.models[model_name].types[type_name] = SourceHeatMap({})
+        if source_name not in self.models[model_name].types[type_name].sources:
+            self.models[model_name].types[type_name].sources[source_name] = {}
+        if value_name not in self.models[model_name].types[type_name].sources[source_name]:
+            self.models[model_name].types[type_name].sources[source_name][value_name] = 0
+        self.models[model_name].types[type_name].sources[source_name][value_name] += value
+    def append_value(self, model_name: str, type_name: str, source_name: str, value_name: str, value: Any):
+        if model_name not in self.models:
+            self.models[model_name] = TypeHeatMap()
+        if type_name not in self.models[model_name].types:
+            self.models[model_name].types[type_name] = SourceHeatMap({})
+        if source_name not in self.models[model_name].types[type_name].sources:
+            self.models[model_name].types[type_name].sources[source_name] = {}
+        if value_name not in self.models[model_name].types[type_name].sources[source_name]:
+            self.models[model_name].types[type_name].sources[source_name][value_name] = []
+        self.models[model_name].types[type_name].sources[source_name][value_name].append(value)
+    def get_heatmap(self, model_name: str, value_name: str):
+        dict_values = self.models[model_name]
+        y_labels = list(dict_values.types.keys())
+        x_labels = list(dict_values.types[y_labels[0]].sources.keys())
+        values: list[list[Any]] = []
+        for type_name in y_labels:
+            x_values = []
+            for source_name in x_labels:
+                if value_name not in dict_values.types[type_name].sources[source_name]:
+                    x_values.append(0)
+                elif source_name not in dict_values.types[type_name].sources:
+                    x_values.append(0)
+                elif type_name not in dict_values.types:
+                    x_values.append(0)
+                else:
+                    x_values.append(self.models[model_name].types[type_name].sources[source_name][value_name])
+            values.append(x_values)
+        return y_labels, x_labels, values
+    def __json__(self):
+        return self.__dict__
+    
+    @staticmethod
+    def _load_from_json(values):
+        instance = ModelHeatMap()
+        for model_name, model_values in values["models"].items():
+            for type_name, type_values in model_values["types"].items():
+                for source_name, source_values in type_values["sources"].items():
+                    for value_name, value in source_values.items():
+                        instance.set_value(model_name, type_name, source_name, value_name, value)
+        return instance
+    
+    
+    @staticmethod
+    def read_json(file_name) -> "ModelHeatMap":
+        with open(file_name, "r", encoding="utf-8") as json_file:
+            data = json.load(json_file)
+        return ModelHeatMap._load_from_json(data)
+    
+    def save_json(self, file_name):
+        with open(file_name, "w", encoding="utf-8") as json_file:
+            json.dump(self, json_file, default=lambda o: o.__json__(), indent=4)
+
+class TypeHeatMap:
+    def __init__(self, types: dict[str, "SourceHeatMap"] | None = None) -> None:
+        if types is None:
+            self.types: dict[str, SourceHeatMap] = {}
+        else:
+            self.types = types
+    def __json__(self):
+        return self.__dict__
+
+class SourceHeatMap:
+    def __init__(self, values: dict[str, Any]) -> None:
+        self.sources: dict[str, Any] = {}
+    def __json__(self):
+        return self.__dict__
